bba5d0925eb58be32a0c5c702ad9c5172f0c5c81
adriraj2000 <adrirajc@gmail.com>
2021-02-26 18:55:08 +0530

    little tweaks

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -10,7 +10,7 @@ export const setToken = token => {
 };
 
 export const call = async(method,path,data) => {
-    const response = await axios[method](`api/${path}`,data);
+    const response = await axios[method](`/api/${path}`,data);
     return response.data;
 }
 
